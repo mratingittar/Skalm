e51b99a39c5a822853660210e85f0d08bcbe51ef
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-10-04 14:28:56 +0200

    Standardized Player Move Input Classes

---
+++ OOP2_Projektarbete/Classes/Input/IInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using OOP2_Projektarbete.Classes.Structs;
+
+namespace OOP2_Projektarbete.Classes.Input
+{
+    internal interface IMoveInput
+    {
+        public Vector2Int GetMoveInput();
+    }
+}
