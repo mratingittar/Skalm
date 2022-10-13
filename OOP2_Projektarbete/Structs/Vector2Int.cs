ebb0fe2619d65a5031cfd883bcea886f8157aa1d
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-10-13 13:08:51 +0200

    Vector2Int Equals Operator Overloaded

--- OOP2_Projektarbete/Structs/Vector2Int.cs
+++ OOP2_Projektarbete/Structs/Vector2Int.cs
@@ -18,5 +18,10 @@ namespace Skalm.Structs
         }
 
         public static Vector2Int Zero { get => new Vector2Int(0, 0); }
+
+        public static bool Equals(Vector2Int a, Vector2Int b)
+        {
+            return ((a.X == b.X) && (a.Y == b.Y));
+        }
     }
 }
