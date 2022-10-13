844adc1ad2cb6f93f1c7a7b03f5fdcb2285e2d34
mratingittar <54244741+mratingittar@users.noreply.github.com>
2022-10-13 14:06:08 +0200

    Created Spawning Factories.

---
+++ OOP2_Projektarbete/Actors/Spawning/ItemSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skalm.Actors.Spawning
+{
+    internal class ItemSpawner : ISpawner<Item>
+    {
+        public Item Spawn()
+        {
+            Console.WriteLine("Item Spawned");
+            return new Item();
+        }
+    }
+}
---
+++ OOP2_Projektarbete/Actors/Spawning/MonsterSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skalm.Actors.Spawning
+{
+    internal class MonsterSpawner : ISpawner<Monster>
+    {
+        public Monster Spawn()
+        {
+            Console.WriteLine("Monster Spawned");
+            return new Monster();
+        }
+    }
+}
